3375b33f20b78d1e520e0d4d8fdb8c1872a22770
pratham14527 <pratham.r.patel@nuv.ac.in>
2024-09-23 12:24:14 +0530

    Add files via upload

---
+++ associative_array.php
@@ -0,0 +1,7 @@
+<?php
+//first way
+$sdata=array("sid"=>101,"sname"=>"pratham","sadd"=>"akota");
+echo $sdata["sid"]."<br>";
+echo $sdata["sname"]."<br>";
+echo $sdata["sadd"]."<br>";
+?>
---
+++ index_array.php
@@ -0,0 +1,14 @@
+<?php
+//first way
+$sname =array("a1","b1","c1");
+echo $sname[0]."<br>";
+echo $sname[1]."<br>";
+echo $sname[2]."<br>";
+//second way
+$rno[0]=101;
+$rno[1]=102;
+$rno[2]=103;
+echo $rno[0]."<br>";
+echo $rno[1]."<br>";
+echo $rno[2]."<br>";
+?>
