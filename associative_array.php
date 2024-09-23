bf7c42c9181c0011a301be2fcdf334b2dd07148d
pratham14527 <pratham.r.patel@nuv.ac.in>
2024-09-23 12:28:29 +0530

    Add files via upload

--- associative_array.php
+++ associative_array.php
@@ -4,4 +4,11 @@ $sdata=array("sid"=>101,"sname"=>"pratham","sadd"=>"akota");
 echo $sdata["sid"]."<br>";
 echo $sdata["sname"]."<br>";
 echo $sdata["sadd"]."<br>";
+//second way
+$edata["eid"]=101;
+$edata["dept"]="sales";
+$edata["salary"]=25000;
+echo"eid=".$edata["eid"]."<br>";
+echo"department=".$edata["dept"]."<br>";
+echo"salary=".$edata["salary"]."<br>";
 ?>
