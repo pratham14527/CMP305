206fdae1a55aa4af7097c4d01c688d4c625a7a19
pratham14527 <pratham.r.patel@nuv.ac.in>
2024-09-30 15:49:34 +0530

    Add files via upload

---
+++ f2.php
@@ -0,0 +1,14 @@
+<?php
+$uname=$_GET['uname'];
+$address=$_GET['address'];
+$email=$_GET['email'];
+$pass=$_GET['pass'];
+$m=$_GET['mobileno'];
+$gender=$_GET['gender'];
+echo "<br>my current fullname is :".$uname;
+echo " <br>my current address is:".$address;
+echo "<br>my current email is:".$email;
+echo "<br> my current password is :".$pass; 
+echo "<br>my current mobileno is:".$m;
+echo"<br>my current gender is:".$gender;
+?>
