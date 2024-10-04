43e17fa7544ad14e684f8f435fd96917795571ef
Pratham PateL <pratham.r.patel@nuv.ac.in>
2024-10-04 10:27:33 +0530

    Add files via upload

---
+++ food_data.php
@@ -0,0 +1,12 @@
+<?php
+$fname=$_GET['fname'];
+$ftype=$_GET['ftype'];
+include "header.html";
+
+echo "<br><br>food name is:".$fname;
+echo "<br><br> food type is:".$ftype;
+
+echo"<br><br><br>";
+
+include "footer.html";
+?>
